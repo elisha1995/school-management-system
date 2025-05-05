f32b515e017887e751b3071ad7110b3a989d7f4a
elisha1995 <elishasamuel01@gmail.com>
2025-05-05 20:16:52 +0000

    Revamps admin dashboard and updates dependencies
    Redesigns admin dashboard layout with responsive user cards
    Adds new Menu and Navbar components for improved navigation
    Introduces custom Tailwind colors for consistent styling
    Updates project name and dependencies, including Recharts integration
    Enhances package organization and structure

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image";
+import Link from "next/link";
+import Menu from '@/components/Menu';
+import Navbar from '@/components/Navbar';
 
 export default function DashboardLayout({
   children,
@@ -7,13 +11,21 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/* LEFT */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
-        l
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2"
+        >
+          <Image src="/logo.png" alt="Logo" width={32} height={32} />
+          <span className="hidden lg:block">GES-School</span>
+        </Link>
+        <Menu />
       </div>
       {/* RIGHT */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">
-        r
-      </div> 
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
+        <Navbar />
+        {children}
+      </div>
     </div>
-  );
+  ); 
 }
