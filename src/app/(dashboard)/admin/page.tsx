aed86e7ba72fa1ee4a6ad1bd851f52b9c8b7b7be
elisha1995 <elishasamuel01@gmail.com>
2025-05-05 20:38:29 +0000

    Adds dashboard charts and event calendar integration
    Integrates new components for admin dashboard, including charts for attendance, finance, and user counts. Adds event calendar and announcements section for improved data visualization and user engagement. Updates dependencies to support new features, including `react-calendar`.
    
    Addresses improved UI and functionality requirements.

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,8 @@
+import Announcements from "@/components/Announcements";
+import AttendanceChart from "@/components/AttendanceChart";
+import CountChart from "@/components/CountChart";
+import EventCalendar from "@/components/EventCalendar";
+import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 
 const AdminPage = () => {
@@ -12,11 +17,29 @@ const AdminPage = () => {
           <UserCard type="parent" />
           <UserCard type="staff" />
         </div>
+        {/* MIDDLE CHARTS */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* COUNT CHART */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          {/* ATTENDANCE CHART */}
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendanceChart />
+          </div>
+        </div>
+        {/* BOTTOM CHART */}
+        <div className="w-full h-[500px]">
+          <FinanceChart />
+        </div>
       </div>
       {/* RIGHT */}
-      <div className="w-full lg:w-1/3 flex flex-col gap-8">r</div>
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
     </div>
   );
-}
+};
 
 export default AdminPage;
