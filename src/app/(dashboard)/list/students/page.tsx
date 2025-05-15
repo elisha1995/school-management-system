9c28bfbee20a77f6ce5786c52246078d5e03adef
elisha1995 <elishasamuel01@gmail.com>
2025-05-15 12:23:58 +0000

    fix: update FormModal ID prop type to accept both number and string for student and teacher deletion

--- src/app/(dashboard)/list/students/page.tsx
+++ src/app/(dashboard)/list/students/page.tsx
@@ -75,7 +75,7 @@ const renderRow = (item: StudentList) => (
           // <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
           //   <Image src="/delete.png" alt="" width={16} height={16} />
           // </button>
-          <FormModal table="student" type="delete" id={Number(item.id)} />
+          <FormModal table="student" type="delete" id={item.id} />
         )}
       </div>
     </td>
